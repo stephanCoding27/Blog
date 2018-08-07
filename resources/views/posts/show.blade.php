d6f71e9e853402f417a2099eff571a30e715d98e
stephanCoding27 <stephan27remnae95@gmail.com>
2018-08-07 16:09:33 +0200

    setting dates and edit and delete buttons for posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -4,8 +4,37 @@
 
 @section('content')
 
-	<h1>{{ $post->title }}</h1>
+	<div class="row">
+		<div class="col-md-8">
+			<h1>{{ $post->title }}</h1>
 
-	<p class="lead">{{ $post->body }} </p>
+			<p class="lead" style="word-wrap: break-word;">{{ $post->body }} </p>
+		</div> {{-- 8 col --}}
+
+		<div class="col-md-4">
+			<div class="card bg-light mb-3 " style="max-width: 35rem;">
+				<dl class="dl-horizontal"> 
+					<dt>Created At:</dt>
+					<dd>{{date('M j, Y H:i',strtotime($post->created_at)) }}</dd>
+				</dl>
+
+				<dl class="dl-horizontal">
+					<dt>Last Updated:</dt>
+					<dd>{{date('M j, Y H:i',strtotime($post->updated_at)) }}</dd>
+				</dl>
+				
+				<div class="row">
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.edit','Edit', array($post->id), array('class' => 'btn btn-primary btn-block')) !!}
+					</div>
+					<div class="col-sm-6">
+						{!! Html::linkRoute('posts.destroy', 'Delete', array($post->id), array('class' => 'btn btn-danger btn-block')) !!}
+					</div>
+				</div> {{-- row --}}
+
+			</div> {{-- well --}}
+		</div> {{-- 4 col --}}
+	</div>
+	<hr>
 
 @endsection
